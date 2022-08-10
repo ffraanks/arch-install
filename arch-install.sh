f7da3fcb78c3dbe10c82c85407d93427efd6d61e
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-08-10 09:01:53 -0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -4,17 +4,17 @@
 # Franklin Souza
 # @FranklinTech
 
-init(){
-  clear
-  cat << Warning
+#init(){
+  #clear
+  #cat << Warning
 AVISO: Esse script foi criado/desenvolvido para minha própria instalação tudo aqui foi pensando EM MIM, caso você venha a usa-lo, por favor sinta-se livre em abri-lo e ler o código fonte, NÃO ME RESPOSABILIZO POR DANOS, caso queira instalar o Arch Linux, recomendo a documentção oficial: https://wiki.archlinux.org/title/Installation_guide_(Portugu%C3%AAs)
 Eu utilizo o Filesystem BTRFS minha montagem é simples:
 
 dev/sda1 - EFI - BOOT
 dev/sda2 - / (BTRFS)
-Warning
-  printf "\n\n" && read -p 'PRESSIONE ENTER PARA CONTINUAR...'
-}
+#Warning
+  #printf "\n\n" && read -p 'PRESSIONE ENTER PARA CONTINUAR...'
+#}
 
 font_system(){
   clear
@@ -74,18 +74,17 @@ fstab_gen(){
   genfstab /mnt >> /mnt/etc/fstab
 }
 
-arch-chroot_enter(){
+arch_chroot_enter(){
   clear
   echo -e "[!] - Entrando no chroot\n"
   sleep 2
   arch-chroot /mnt
 }
 
-init
 font_system
 format_disk
 subvolumes
 mount_partitions
 pacstrap_arch
 fstab_gen
-arch-chroot_enter
+arch_chroot_enter
